6333ae7209910c461cbd7ab38ea22b48ab871c5b
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2024-12-09 21:35:14 -0300

    definindo propriedades da pagina inicial e criando componente

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,16 @@
+import { Welcome } from "@/components/welcome"
 import { View, Text } from "react-native"
 
 
 export default function Index() {
 	return (
-		<View>
-			<Text>Hello Mobile!!</Text>
+		<View style={{
+			flex: 1,
+			padding: 40,
+			gap: 40
+		}}>
+			<Welcome />
 		</View>
 	)
- }
+}
 
