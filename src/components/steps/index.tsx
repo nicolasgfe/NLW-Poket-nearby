89631103ccb9c748ae0ca8d356fc5fa673b2d7f8
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2024-12-09 22:45:06 -0300

    Adicionando propriedades no componente com icones

--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 
 import { s } from "./styles"
 import { Step } from "../step";
+import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
 
 export function Steps() {
 	return (
@@ -9,14 +10,17 @@ export function Steps() {
 			<Text style={s.title}>Veja como fuinciona:</Text>
 
 			<Step
+				icon={IconMapPin}
 				title="Encontre estabelecimentos"
 				description="Veja locais perto de você que são parceiros Nearby"
 			/>
 			<Step
+			icon={IconQrcode}
 				title="Ative o cupom com QR Code"
 				description="Escaneie o código no estabelecimento para usar o benefício"
 			/>
 			<Step
+			icon={IconTicket}
 				title="Garanta vantagens perto de você"
 				description="Ative cupons onde estiver, em diferentes tipos de estabelecimento "
 			/>
