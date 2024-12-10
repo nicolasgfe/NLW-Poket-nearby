25f830ed38610fd63bd6365f075d5a7de6e63a7f
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2024-12-10 20:56:13 -0300

    adicionando rota Home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { View } from "react-native"
+import { router } from "expo-router"
 
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
@@ -14,7 +15,7 @@ export default function Index() {
 		}}>
 			<Welcome />
 			<Steps />
-			<Button isLoading>
+			<Button onPress={() => router.navigate("/home")}>
 				<Button.Title>Começar </Button.Title>
 			</Button>
 		</View>
